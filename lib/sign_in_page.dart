b813e7149c6e888f6e788d41d6490f870470030e
NelsonTejeda <nelsonjesustejeda@gmail.com>
2020-10-18 18:18:43 -0400

    login page clean up

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -11,36 +11,57 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          Image.asset('assets/images/logohighrez.png'),
           TextField(
             controller: emailController,
             decoration: InputDecoration(
-              labelText: "Email",
+                hintText: "Email",
+                contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
             ),
+          ),
+          SizedBox(
+            width: 200.0,
+            height: 20.0,
           ),
           TextField(
             controller: passwordController,
             decoration: InputDecoration(
-              labelText: "Password",
+              hintText: "Password",
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
             ),
           ),
-          RaisedButton(
-            onPressed: () {
-              context.read<AuthenticationHelper>().signIn(
-                email: emailController.text.trim(),
-                password: passwordController.text.trim(),
-              );
-            },
-            child: Text("Sign in"),
-          ),
-          RaisedButton(
-            onPressed: () {
-              context.read<AuthenticationHelper>().signUp(
+          Container(
+            width: 300.0,
+              child: RaisedButton(
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                color: Color(0xFFE89696),
+                onPressed: () {
+                  context.read<AuthenticationHelper>().signIn(
                   email: emailController.text.trim(),
                   password: passwordController.text.trim(),
-              );
-            },
-            child: Text("Sign Up"),
-          )
+                  );
+                },
+                child: Text("Sign in"),
+          )),
+          Container(
+            width: 300.0,
+              child: RaisedButton(
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                color: Colors.green,
+                onPressed: () {
+                  context.read<AuthenticationHelper>().signUp(
+                  email: emailController.text.trim(),
+                  password: passwordController.text.trim(),
+                  );
+                },
+                child: Text("Sign Up"),
+          ))
         ],
       ),
     );
