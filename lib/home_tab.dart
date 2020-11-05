c00c2b97c3dcf24d9f95135ed23e072141db686e
NelsonTejeda <nelsonjesustejeda@gmail.com>
2020-11-04 21:34:33 -0500

    added api to home screen

--- lib/home_tab.dart
+++ lib/home_tab.dart
@@ -6,6 +6,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:take_a_break/Authentication_Helper.dart';
 import 'package:take_a_break/globals.dart';
+import 'package:dio/dio.dart';
 
 class HomeTab extends StatefulWidget {
   @override
@@ -16,10 +17,49 @@ class _HomeTabState extends State<HomeTab> {
   final databaseReference = FirebaseDatabase.instance.reference();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   String username;
+  Future<List<Widget>> news;
+
+  Container newArticle(String title, String img, String description ){
+    return Container(
+      width: 200,
+      height: 200,
+      child: Card(
+        child: Wrap(
+          children: <Widget>[
+            Image.network(img),
+            ListTile(
+              title: Text(title),
+              subtitle: Text(description),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  apiCallTest() async {
+    var response = await Dio().get('https://newsapi.org/v2/top-headlines?country=us&apiKey=5f3442d6b21842528f3eb23490cf9e3d');
+    return response.data["articles"][0]["title"];
+  }
+
+  Future<List<Widget>> makeNewsScroll() async{
+    List<Widget> list = new List<Widget>();
+    var response = await Dio().get('https://newsapi.org/v2/top-headlines?country=us&apiKey=5f3442d6b21842528f3eb23490cf9e3d');
+    for(var i = 0; i < 10; i++){
+      list.add(newArticle(response.data["articles"][i]["title"], response.data["articles"][i]["urlToImage"], response.data["articles"][i]["description"]));
+    }
+    return list;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    news = makeNewsScroll();
+  }
 
   @override
   Widget build(BuildContext context) {
-
     databaseReference.child("${Globals.uid}/username").once().then((DataSnapshot data){
       if(mounted && username != null){
         return;
@@ -80,13 +120,42 @@ class _HomeTabState extends State<HomeTab> {
               height: height * 0.7,
               child: Column(
                 children: <Widget>[
+                  Container(
+                    width: 400,
+                    height: 300,
+                    child: FutureBuilder<List<Widget>>(
+                      future: news,
+                      builder: (context, snapshot){
+                        return snapshot.hasData ?
+                        ListView(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          children: snapshot.data,
+                        ) :
+                        ListView(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            newArticle("loading", "https://i.pinimg.com/originals/6d/73/4e/6d734e72142078bc2b1994ef80902d9b.gif", "..."),
+                            newArticle("loading", "https://i.pinimg.com/originals/6d/73/4e/6d734e72142078bc2b1994ef80902d9b.gif", "..."),
+                            newArticle("loading", "https://i.pinimg.com/originals/6d/73/4e/6d734e72142078bc2b1994ef80902d9b.gif", "...")
+                          ],
+                        );
+                      },
+                    ),
+                  ),
                   RaisedButton(
                     onPressed: () {
                       context.read<AuthenticationHelper>().signOut();
                     },
                     child: Text("Sign out"),
                   ),
-                  Text("")
+                  RaisedButton(
+                    onPressed: () async {
+                       print(await apiCallTest());
+                    },
+                    child: Text("api call test"),
+                  ),
                 ],
               ),
             ),
